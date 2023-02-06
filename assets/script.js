14b4f976dd69ea3d5fc08b58166ca82450fe7210
Yusif Nuruzade <projectny@example.com>
2023-02-06 07:34:12 -0800

    project created

---
+++ assets/script.js
@@ -0,0 +1,24 @@
+// let a = 0b11111111; // бинарная форма записи числа 255
+// let b = 0o377; // восьмеричная форма записи числа 255
+// alert(255..toString(36));
+
+
+
+
+
+// let num = 12.34;
+// let num1 = num.toFixed(5);
+// console.log( +(num.toFixed(5)) ); 
+
+
+
+
+
+// let str = '100.5px';
+// alert(parseInt(str));
+// alert(parseFloat(str));
+
+
+
+
+
